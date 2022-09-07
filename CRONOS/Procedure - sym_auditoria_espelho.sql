a8f1e7c07d11af372efd514ad426cf1cadabf3a0
lucasrodrigues062 <lucasrodrigues062@gmail.com>
2022-09-07 08:42:03 -0300

    Corrigido Script de procedure

--- CRONOS/Procedure - sym_auditoria_espelho.sql
+++ CRONOS/Procedure - sym_auditoria_espelho.sql
@@ -1,4 +1,7 @@
+IF EXISTS (SELECT 1 from sys.objects where name = 'sym_auditoria_espelho')
 
+	DROP PROCEDURE dbo.sym_auditoria_espelho
+GO
 
 ----------------------------------
 CREATE PROCEDURE	sym_auditoria_espelho
@@ -14,12 +17,12 @@ BEGIN
 	IF EXISTS (SELECT 1 FROM ZMovimentoCompl zc WHERE Idmov = @IdMovimento)
 	
 		BEGIN 
-			UPDATE ZMovimentoCompl SET sym_audit_print = sym_audit_print + @Usuario WHERE IdMov = @IdMovimento
+			UPDATE ZMovimentoCompl SET sym_audit_print = sym_audit_print + @Usuario + ' | ' WHERE IdMov = @IdMovimento
 		END
 	
 	ELSE
 		BEGIN 
-			INSERT INTO ZMovimentoCompl (IdMov, sym_audit_print) VALUES (@IdMovimento, @Usuario)
+			INSERT INTO ZMovimentoCompl (IdMov, sym_audit_print) VALUES (@IdMovimento, @Usuario + ' | ')
 		END
 		
 	SET NOCOUNT OFF
